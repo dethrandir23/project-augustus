6fe483552aa3d31dac5eeda47b2d6b09a620d74b
Bekir Efe Öztürk <efeoztrk23@proton.me>
2026-02-27 00:19:19 +0300

    refactor: Move LocationComponent from Map.h to LocationComponent.h

--- src/World/Components/LocationComponent.h
+++ src/World/Components/LocationComponent.h
@@ -1,16 +1,39 @@
 // src/World/Components/LocationComponent.h
 #include "Core/ECS/Component.h"
 #include "uuid/uuid.h"
+#include "Core/Types.h"
 
+// Note: In the future, map and location features can be generalized as N-dimensional. 
+// This way, there will be no need to deal with adding dimensions manually.
 class LocationComponent : public Component {
 public:
-    // Eğer haritan Grid bazlıysa (x, y) integer
-    // Eğer Dünya haritasıysa (latitude, longitude) double
-    float x = 0.0f;
-    float y = 0.0f;
-    
-    // Eğer bir hiyerarşi varsa (Fabrika -> Şehrin İçinde)
-    uuids::uuid parentId; // Hangi Entity'nin içinde?
+    LocationComponent(double x, double y) {
+        location = Location(x, y);
+    }
 
     std::string GetComponentType() const override { return "LocationComponent"; }
+
+    double GetX() const { return location.x; }
+    double GetY() const { return location.y; }
+
+    void SetX(double x) { location.x = x; }
+    void SetY(double y) { location.y = y; }
+
+    void SetPosition(double x, double y) {
+        location.x = x;
+        location.y = y;
+    }
+
+    void ResetPosition() {
+        location.x = 0;
+        location.y = 0;
+    }
+
+    void Move(double dx, double dy) {
+        location.x += dx;
+        location.y += dy;
+    }
+
+private:
+    Location location;
 };
